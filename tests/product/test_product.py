4e0f04fa087d17561b131a90111dd7fe32179048
Marcos Danilo <marcosdanilo94@gmail.com>
2022-10-28 15:41:48 -0300

    test: :white_check_mark: garante que o inicializado do objeto Produto funciona

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
+from datetime import datetime
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        1,
+        "Nicotine Polacrilex",
+        "Target Corporation",
+        datetime(2021, 2, 18),
+        datetime(2023, 9, 17),
+        "CR25 1551 4467 2549 4402 1",
+        "instrucao 1",
+    )
+    assert product.id == 1
+    assert product.nome_do_produto == "Nicotine Polacrilex"
+    assert product.nome_da_empresa == "Target Corporation"
+    assert product.data_de_fabricacao == str(datetime(2021, 2, 18))
+    assert product.data_de_validade == str(datetime(2023, 9, 17))
+    assert product.numero_de_serie == "CR25 1551 4467 2549 4402 1"
+    assert product.instrucoes_de_armazenamento == "instrucao 1"
