707924575a384f85802c02803ed9d1f9c663288d
Marcos Danilo <marcosdanilo94@gmail.com>
2022-10-28 16:48:54 -0300

    test: :white_check_mark: garante que a class Product tem um método __repr__ implementado

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,29 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
+from datetime import date
+from pytest import fixture
 
 
-def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+product_report_mock = (
+    "O produto Nicotine Polacrilex"
+    f" fabricado em {date(2021, 2, 18)}"
+    " por Target Corporation com validade"
+    f" até {date(2023, 9, 17)}"
+    " precisa ser armazenado instrucao 1."
+)
+
+
+@fixture
+def product():
+    return Product(
+        1,
+        "Nicotine Polacrilex",
+        "Target Corporation",
+        date(2021, 2, 18),
+        date(2023, 9, 17),
+        "CR25 1551 4467 2549 4402 1",
+        "instrucao 1",
+    )
+
+
+def test_relatorio_produto(product):
+    assert product_report_mock == str(product)
